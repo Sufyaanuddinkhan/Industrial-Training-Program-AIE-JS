f971111b49fcefe58e949d2876dfbbb331ad8270
Mohd Sufyaanuddin Khan <khansufyaan315@gmail.com>
2026-08-08 12:19:16 +0530

    Day 1 of JS

---
+++ index.js
@@ -0,0 +1,41 @@
+// let age = 23;
+
+// console.log(typeof (age));
+
+// let num1 = 4
+// let num2 = 12
+
+// let div1 = num1 ** num2
+// let div2 = num1 % num2 //
+
+// console.log(div1)
+
+
+// let num
+// console.log(num)
+// console.log(0 / 0);
+
+// let result = (15 + 20) / 17 + 21 * 20;
+
+// console.log(result)
+
+// let age = 23
+
+// age += 1;
+
+// console.log(age)
+
+// let age = 24
+
+// console.log(age++)
+// console.log(age)
+
+// let x = 6
+// console.log(++x)
+// console.log(x)
+
+let age = 23
+let newage = age + 2
+
+console.log(age)
+console.log(newage)
